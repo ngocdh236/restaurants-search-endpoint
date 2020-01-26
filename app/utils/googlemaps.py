93b89505cbe245cfd8ee32ab0b249738258130e5
Ngoc <ngocdh236@gmail.com>
2020-01-26 21:53:59 +0200

    Add util to get distance between 2 locations on Google maps

---
+++ app/utils/googlemaps.py
@@ -0,0 +1,22 @@
+import googlemaps
+
+
+def get_distance(query_location, restaurant_location):
+    """
+    Return distance between 2 locations on Google maps in kilometers 
+
+    :param tuple query_location: lat/lon of query location
+    :param tuple restaurant_location: lat/lon of restaurant location
+    :return float:
+    """
+
+    gmaps = googlemaps.Client(key='AIzaSyDN8LXzldXjbZakCCyvUAnJv_bf4s2CgiE')
+    distance_matrix = gmaps.distance_matrix(
+        origins=query_location, destinations=restaurant_location)
+
+    if distance_matrix["status"] == "OK":
+        first_element = distance_matrix["rows"][0]["elements"][0]
+        if first_element["status"] == "OK":
+            return first_element["distance"]["value"] / 1000
+
+    return -1
